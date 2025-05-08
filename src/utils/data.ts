8e2f09ad6ff03ff8753661e5d7f1d2e2c82c0767
KamauHarrison <100772289+kamau-n@users.noreply.github.com>
2025-05-08 23:34:25 +0300

    Update data.ts

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -43,7 +43,7 @@ export const projects: Project[] = [
         "This is an application that assist service provider and service seekers connnect.",
       technologies: ["React Native", "Firebase", "Expo 53","Typescript"],
       imageUrl:
-        "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80",
+        "https://drive.google.com/file/d/18a-8vDNJw2sAEUEJdYP78xsZ99oINX3E/view?usp=drive_link",
       githubUrl: "https://github.com/kamau-n/service-app",
       liveUrl: "kamauharrison.co.ke/",
       category: "app",
